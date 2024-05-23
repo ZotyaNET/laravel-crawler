7d416de6443991b8dda7e6c845c324419ac9777c
Zotya <zoltan.karpat@professionpartner.hu>
2024-05-23 23:48:37 +0200

    Laravel package

--- src/Listeners/RequestSendingListener.php
+++ src/Listeners/RequestSendingListener.php
@@ -1,9 +1,9 @@
 <?php
 
 /*
- * This file is part of the jiannei/laravel-crawler.
+ * This file is part of the pannonpuma/laravel-crawler.
  *
- * (c) jiannei <jiannei@sinan.fun>
+ * (c) Zoltan Karpat <karpat.zoltan@gmail.com>
  *
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
