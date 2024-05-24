0d2eabf1f986b60d816e51e68a0647a0846f8c29
Zotya <zoltan.karpat@professionpartner.hu>
2024-05-24 04:07:33 +0200

    Laravel package

---
+++ src/Console/Commands/Crawl.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * This file is part of the pannonpuma/laravel-crawler.
+ *
+ * (c) Zoltan Karpat <karpat.zoltan@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace PannonPuma\LaravelCrawler\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Contracts\Console\Isolatable;
+use PannonPuma\LaravelCrawler\Support\Facades\Crawler;
+
+class Crawl extends Command implements Isolatable
+{
+    protected $signature = 'crawler:crawl {name?} {--action=run}';
+
+    protected $description = 'Manage crawler tasks.';
+
+    public function handle()
+    {
+        $this->info("[{$this->description}]:starting " . now()->format('Y-m-d H:i:s'));
+
+        match ($this->option('action')) {
+            default => $this->runTask()
+        };
+
+        $this->info("[{$this->description}]:finished " . now()->format('Y-m-d H:i:s'));
+    }
+
+    protected function runTask()
+    {
+        dispatch(Crawler::curl([]));
+    }
+
+    protected function import()
+    {
+        $this->comment('importing...');
+
+        Crawler::source('database', Crawler::source()->all());
+    }
+
+    protected function export()
+    {
+        $this->comment('exporting...');
+
+        Crawler::source('file', Crawler::source('database')->all());
+    }
+}
